74fc0c6d20456d6d318ea0f43ce6a4bfd0fbae2a
aozdemir <aozdemir@webmd.net>
2018-11-08 23:33:43 -0500

    command

---
+++ apps/app-docs-demo/AutofacReadthedocsIo/register/registration/defaultRegistrations/IntroTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AutofacReadthedocsIo.register.registration.defaultRegistrations
+{
+    /// <summary>
+    /// https://autofac.readthedocs.io/en/latest/register/registration.html#default-registrations
+    /// </summary>
+    [TestClass]
+    public class IntroTests
+    {
+        [TestMethod]
+        public void Intro()
+        {
+
+        }
+    }
+}
